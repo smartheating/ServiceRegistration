f1c1ce9184d195145f7147c7ae2c622f5fcb17bf
Pascal Weiß <pa-weiss@gmx.de>
2019-06-01 09:43:33 +0200

    Changed dockerfile such that container can be started right away (without building jar in advance)

--- run-docker.sh
+++
@@ -1,8 +0,0 @@
-echo "Creating new JAR-File..."
-mvn clean install
-echo "Removing current container..."
-docker container rm -f serviceregistration
-echo "Removing current image..."
-docker image rm -f serviceregistration
-echo "Building new image and starting container..."
-docker-compose up -d
