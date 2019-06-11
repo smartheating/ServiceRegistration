89f208b225074bdecd8b70373ed2b1fa764bd2c2
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-11 17:14:25 +0200

    Service registration is now logging information about all instances every 30 seconds

--- src/main/java/de/smartheating/eureka/ServiceRegistrationApplication.java
+++ src/main/java/de/smartheating/eureka/ServiceRegistrationApplication.java
@@ -3,9 +3,11 @@ package de.smartheating.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableScheduling
 public class ServiceRegistrationApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/de/smartheating/eureka/scheduling/ScheduledOperations.java
@@ -0,0 +1,34 @@
+package de.smartheating.eureka.scheduling;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.shared.Application;
+import com.netflix.eureka.EurekaServerContextHolder;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+
+@Service
+public class ScheduledOperations {
+	
+	Logger logger = LoggerFactory.getLogger(ScheduledOperations.class);
+
+	@Scheduled(fixedDelayString = "30000")
+	public void logInstances() {
+		PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
+		List<Application> applications = registry.getApplications().getRegisteredApplications();
+		
+		for (Application application: applications) {
+			List<InstanceInfo> infos = application.getInstances();
+			for (InstanceInfo info: infos) {
+				logger.info("AppName: " + info.getAppName() + "; InstanceID: " + info.getInstanceId() + "; URL:" + info.getHomePageUrl());
+			}
+		}
+		
+		logger.info("------------------------Break-----------------------");
+	}
+}
